781b9c59a97179ea95457067225d3cb0e75a726a
Paras <paraspatel1434@gmail.com>
2022-12-06 19:06:19 +0530

    sentry added

---
+++ sunbird-rc-ED-ui/src/app/sentry.ts
@@ -0,0 +1,27 @@
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from "@sentry/tracing";
+
+import { AppModule } from "./app.module";
+
+Sentry.init({
+  dsn: "https://85c4f20a5c9a4d29bee0f04787174cd3@o4504281638567936.ingest.sentry.io/4504281991086080",
+  integrations: [
+    new BrowserTracing({
+      tracePropagationTargets: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(err => console.error(err));
