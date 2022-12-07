c63f6d6d46b7fc3c1a895ffe8a897fa25d4cb3d9
shivani parit <shivani_p@techjoomla.com>
2022-12-07 18:58:04 +0530

    Test dev deployment

--- sunbird-rc-ED-ui/src/app/documents/add-document/add-document.component.ts
+++ sunbird-rc-ED-ui/src/app/documents/add-document/add-document.component.ts
@@ -3,7 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { FormlyJsonschema } from '@ngx-formly/core/json-schema';
-import { JSONSchema7 } from "json-schema";
+import { JSONSchema7 } from 'json-schema';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { Observable, Observer, Subject } from 'rxjs';
 import { GeneralService } from '../../services/general/general.service';
@@ -13,21 +13,21 @@ import { ToastMessageService } from 'src/app/services/toast-message/toast-messag
 @Component({
   selector: 'app-add-document',
   templateUrl: './add-document.component.html',
-  styleUrls: ['./add-document.component.scss']
+  styleUrls: ['./add-document.component.scss'],
 })
 export class AddDocumentComponent implements OnInit {
-  header = 'documents'
+  header = 'documents';
   form2: FormGroup;
   model = {};
 
   options: FormlyFormOptions;
   fields: FormlyFieldConfig[];
   schema: JSONSchema7 = {
-    "type": "object",
-    "title": "",
-    "definitions": {},
-    "properties": {},
-    "required": []
+    type: 'object',
+    title: '',
+    definitions: {},
+    properties: {},
+    required: [],
   };
   schemaloaded = false;
   osid: any;
@@ -37,8 +37,15 @@ export class AddDocumentComponent implements OnInit {
   certificate: any;
   entity: any;
   isScan: boolean = false;
-  property: { name: { type: string; title: string; }; fileUrl: { type: string; title: string; widget: { formlyConfig: { type: string; }; }; }; };
-  width: any = "100";
+  property: {
+    name: { type: string; title: string };
+    fileUrl: {
+      type: string;
+      title: string;
+      widget: { formlyConfig: { type: string } };
+    };
+  };
+  width: any = '100';
   imageFile: File;
   step = 0;
   steps_length = 0;
@@ -49,7 +56,13 @@ export class AddDocumentComponent implements OnInit {
   policyName: any;
   attestationOSID: any;
   mySubscription: any;
-  constructor(private route: ActivatedRoute,public toastMsg: ToastMessageService, public generalService: GeneralService, private formlyJsonschema: FormlyJsonschema, public router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    public toastMsg: ToastMessageService,
+    public generalService: GeneralService,
+    private formlyJsonschema: FormlyJsonschema,
+    public router: Router
+  ) {}
 
   public webcamImage: WebcamImage = null;
   handleImage(webcamImage: WebcamImage) {
@@ -60,22 +73,24 @@ export class AddDocumentComponent implements OnInit {
       const imageBlob: Blob = blob;
       const imageName: string = this.generateName();
       this.imageFile = new File([imageBlob], imageName, {
-        type: "image/jpeg"
+        type: 'image/jpeg',
       });
-      console.log(this.imageFile)
+      console.log(this.imageFile);
       // this.model['fileUrl'] = imageFile;
     });
   }
 
   dataURItoBlob(dataURI: string): Observable<Blob> {
     return Observable.create((observer: Observer<Blob>) => {
-      const byteString: string = window.atob(dataURI.replace(/^data:image\/(png|jpeg|jpg);base64,/, ''));
+      const byteString: string = window.atob(
+        dataURI.replace(/^data:image\/(png|jpeg|jpg);base64,/, '')
+      );
       const arrayBuffer: ArrayBuffer = new ArrayBuffer(byteString.length);
       const int8Array: Uint8Array = new Uint8Array(arrayBuffer);
       for (let i = 0; i < byteString.length; i++) {
         int8Array[i] = byteString.charCodeAt(i);
       }
-      const blob = new Blob([int8Array], { type: "image/jpeg" });
+      const blob = new Blob([int8Array], { type: 'image/jpeg' });
       observer.next(blob);
       observer.complete();
     });
@@ -83,20 +98,20 @@ export class AddDocumentComponent implements OnInit {
 
   generateName(): string {
     const date: number = new Date().valueOf();
-    let text: string = "";
+    let text: string = '';
     const possibleText: string =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < 5; i++) {
       text += possibleText.charAt(
         Math.floor(Math.random() * possibleText.length)
       );
     }
     // Replace extension according to your media type like this
-    return date + "." + text + ".jpeg";
+    return date + '.' + text + '.jpeg';
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       console.log(params);
       if (params['type']) {
         this.docType = params['type'];
@@ -113,7 +128,7 @@ export class AddDocumentComponent implements OnInit {
       } else {
         this.createStaticForm();
       }
-    })
+    });
   }
 
   ngAfterViewInit() {
@@ -130,25 +145,42 @@ export class AddDocumentComponent implements OnInit {
   createStaticForm() {
     if (this.docType === 'scan') {
       var schema: any = {
-        "name": { "title": this.generalService.translateString('NAME_OF_DOCUMENT'), "type": "string" }
+        name: {
+          title: this.generalService.translateString('NAME_OF_DOCUMENT'),
+          type: 'string',
+        },
       };
       this.schema['properties'] = schema;
       this.schema['properties']['name']['widget'] = {};
       this.schema['properties']['name']['widget']['formlyConfig'] = {};
-      this.schema['properties']['name']['widget']['formlyConfig']['templateOptions'] = { required: true };
+      this.schema['properties']['name']['widget']['formlyConfig'][
+        'templateOptions'
+      ] = { required: true };
     } else {
       var schema: any = {
-        "name": { "title": this.generalService.translateString('NAME_OF_DOCUMENT'), "type": "string" },
-        "fileUrl": { "title": this.generalService.translateString('DOCUMENT'), "type": "string" },
+        name: {
+          title: this.generalService.translateString('NAME_OF_DOCUMENT'),
+          type: 'string',
+        },
+        fileUrl: {
+          title: this.generalService.translateString('DOCUMENT'),
+          type: 'string',
+        },
       };
 
       this.schema['properties'] = schema;
       this.schema['properties']['name']['widget'] = {};
       this.schema['properties']['fileUrl']['widget'] = {};
       this.schema['properties']['name']['widget']['formlyConfig'] = {};
-      this.schema['properties']['fileUrl']['widget']['formlyConfig'] = { "type": "file" };
-      this.schema['properties']['name']['widget']['formlyConfig']['templateOptions'] = { required: true };
-      this.schema['properties']['fileUrl']['widget']['formlyConfig']['templateOptions'] = { required: true };
+      this.schema['properties']['fileUrl']['widget']['formlyConfig'] = {
+        type: 'file',
+      };
+      this.schema['properties']['name']['widget']['formlyConfig'][
+        'templateOptions'
+      ] = { required: true };
+      this.schema['properties']['fileUrl']['widget']['formlyConfig'][
+        'templateOptions'
+      ] = { required: true };
     }
 
     this.form2 = new FormGroup({});
@@ -160,152 +192,149 @@ export class AddDocumentComponent implements OnInit {
 
   createDynamicForm() {
     var search = {
-      "entityType": [
-        "Issuer"
-      ],
-      "filters": {
-        "osid": {
-          "eq": this.osid
+      entityType: ['Issuer'],
+      filters: {
+        osid: {
+          eq: this.osid,
+        },
+      },
+    };
+    this.generalService.postData('/Issuer/search', search).subscribe(
+      (res) => {
+        console.log('pub res', res);
+        this.doc_data = res[0];
+        this.policyName = res[0]['policyName'];
+        if (this.doc_data['steps'] && this.doc_data['steps'].length > 0) {
+          this.steps_length = this.doc_data['steps'].length;
+          this.schema_property = JSON.parse(
+            this.doc_data['steps'][this.step]['form']
+          );
+        } else {
+          this.schema_property = JSON.parse(this.doc_data['additionalInput']);
         }
-      }
-    }
-    this.generalService.postData('/Issuer/search', search).subscribe((res) => {
-      console.log('pub res', res);
-      this.doc_data = res[0];
-      this.policyName = res[0]['policyName']
-      if (this.doc_data['steps'] && this.doc_data['steps'].length > 0) {
-        this.steps_length = this.doc_data['steps'].length;
-        this.schema_property = JSON.parse(this.doc_data['steps'][this.step]['form'])
-      } else {
-        this.schema_property = JSON.parse(this.doc_data['additionalInput']);
-      }
 
-      this.certificate = this.doc_data['title'];
-      this.schema.properties = this.schema_property;
-      this.form2 = new FormGroup({});
-      this.options = {};
-      this.fields = [this.formlyJsonschema.toFieldConfig(this.schema)];
-      this.schemaloaded = true;
-      console.log(this.schema)
-      // this.documentTypes = res;
-    }, (err) => {
-      // this.toastMsg.error('error', err.error.params.errmsg)
-      console.log('error', err)
-    });
+        this.certificate = this.doc_data['title'];
+        this.schema.properties = this.schema_property;
+        this.form2 = new FormGroup({});
+        this.options = {};
+        this.fields = [this.formlyJsonschema.toFieldConfig(this.schema)];
+        this.schemaloaded = true;
+        console.log(this.schema);
+        // this.documentTypes = res;
+      },
+      (err) => {
+        // this.toastMsg.error('error', err.error.params.errmsg)
+        console.log('error', err);
+      }
+    );
   }
 
-
   submit() {
-
     if (this.steps_length > 0 && this.steps_length != this.step) {
       var api = this.doc_data['steps'][this.step]['api'];
-      this.doc_data['steps'][this.step]['request'] = this.model
+      this.doc_data['steps'][this.step]['request'] = this.model;
       this.generalService.postData(api, this.model).subscribe((res) => {
         console.log('api res', res);
         this.doc_data['steps'][this.step]['response'] = res;
 
         this.step++;
-        console.log(this.steps_length, this.step)
+        console.log(this.steps_length, this.step);
         if (this.steps_length == this.step) {
           this.schemaloaded = false;
-          this.schema_property = JSON.parse(this.doc_data['additionalInput'])
+          this.schema_property = JSON.parse(this.doc_data['additionalInput']);
           for (var [key, value] of Object.entries(this.schema_property)) {
             console.log(key, value);
-            if (value["value"]) {
-              var datavalue = this.getValue(value["value"], this.doc_data);
-              console.log("datavalue", datavalue)
-              this.schema_property[key]["value"] = datavalue;
+            if (value['value']) {
+              var datavalue = this.getValue(value['value'], this.doc_data);
+              console.log('datavalue', datavalue);
+              this.schema_property[key]['value'] = datavalue;
               this.schema_property[key]['widget'] = {
-                "formlyConfig": {
-                  "defaultValue": datavalue,
-                  "templateOptions": {
+                formlyConfig: {
+                  defaultValue: datavalue,
+                  templateOptions: {
                     required: true,
-                    disabled: true
+                    disabled: true,
                   },
-                  "expressionProperties": {
-                  },
-                  "validation": {},
-                  "modelOptions": {}
-                }
-              }
-              if (this.schema_property[key]["hidden"]) {
-                this.schema_property[key]['widget']['type'] = 'input'
-                this.schema_property[key]['widget']['formlyConfig']['templateOptions']['type'] = 'hidden' ;
+                  expressionProperties: {},
+                  validation: {},
+                  modelOptions: {},
+                },
+              };
+              if (this.schema_property[key]['hidden']) {
+                this.schema_property[key]['widget']['type'] = 'input';
+                this.schema_property[key]['widget']['formlyConfig'][
+                  'templateOptions'
+                ]['type'] = 'hidden';
               }
             }
-
-          };
+          }
           this.schema.properties = this.schema_property;
           this.form2 = new FormGroup({});
           this.options = {};
           this.fields = [this.formlyJsonschema.toFieldConfig(this.schema)];
-          console.log("schema", this.schema_property)
+          console.log('schema', this.schema_property);
           this.schemaloaded = true;
           this.loading = false;
         }
       });
-
     } else {
       if (!this.osid) {
         var formData = new FormData();
         var file;
         if (this.docType === 'scan') {
           file = this.imageFile;
-          formData.append("files", file);
+          formData.append('files', file);
         } else if (this.model['fileUrl'] && this.model['name']) {
-
           this.loading = true;
           file = this.model['fileUrl'];
-          formData.append("files", file[0]);
+          formData.append('files', file[0]);
 
           this.generalService.getData(this.entity).subscribe((res) => {
-            var url = [this.entity, res[0]['osid'], 'attestation', 'documents']
-            this.generalService.postData(url.join('/'), formData).subscribe((res2) => {
-              this.model['fileUrl'] = res2['documentLocations'];
-              var attest = {
-                "name": "attestation-SELF",
-                "entityName": "User",
-                "entityId": res[0]['osid'],
-                "additionalInput": this.model
-              }
-              console.log(attest);
-              this.postData('send', attest);
-            })
-          })
+            var url = [this.entity, res[0]['osid'], 'attestation', 'documents'];
+            this.generalService
+              .postData(url.join('/'), formData)
+              .subscribe((res2) => {
+                this.model['fileUrl'] = res2['documentLocations'];
+                var attest = {
+                  name: 'attestation-SELF',
+                  entityName: 'User',
+                  entityId: res[0]['osid'],
+                  additionalInput: this.model,
+                };
+                console.log(attest);
+                this.postData('send', attest);
+              });
+          });
         }
-      }
-      else {
+      } else {
         this.generalService.getData(this.entity).subscribe((res) => {
           this.model['title'] = this.certificate;
           var attest = {
-            "name": this.policyName,
-            "entityName": "User",
-            "entityId": res[0]['osid'],
-            "additionalInput": this.model
-          }
+            name: this.policyName,
+            entityName: 'User',
+            entityId: res[0]['osid'],
+            additionalInput: this.model,
+          };
           console.log(attest);
           this.postData('send', attest);
         });
       }
     }
-
   }
 
   getValue(value, steps) {
     var data = value.split('.');
     var result;
-    data.forEach(element => {
-      var num = this.isNumeric(element)
+    data.forEach((element) => {
+      var num = this.isNumeric(element);
       if (num) {
         try {
-          element = parseInt(element)
+          element = parseInt(element);
+        } catch (err) {
+          element = element;
         }
-        catch (err) {
-          element = element
-        }
-
       }
-      steps = steps[element]
+      steps = steps[element];
     });
 
     return steps;
@@ -315,47 +344,52 @@ export class AddDocumentComponent implements OnInit {
     return /\d/.test(_input);
   }
 
-
   postData(url, data) {
-    if(this.doc_data && this.doc_data['logoUrl']){
-      data['logoUrl'] = this.doc_data['logoUrl']
+    if (this.doc_data && this.doc_data['logoUrl']) {
+      data['logoUrl'] = this.doc_data['logoUrl'];
     }
-    this.generalService.postData(url, data).subscribe((res) => {
-      console.log('post res', res);
-      this.attestationOSID = res.result.attestationOSID;
-      // interval(5000).subscribe((x =>{
-      //   this.getPublishedData()
-      // }, 5000));
+    this.generalService.postData(url, data).subscribe(
+      (res) => {
+        console.log('post res', res);
+        this.attestationOSID = res.result.attestationOSID;
+        // interval(5000).subscribe((x =>{
+        //   this.getPublishedData()
+        // }, 5000));
 
-      this.mySubscription=interval(5000).subscribe(x => {
-        console.log("xxx",x)
-        if(x==12){
-          this.router.navigate([this.entity, 'documents'])
-          this.toastMsg.error('error', "Taking more time than usual to fetch document from issuer, something went wrong, please try again")
-          this.mySubscription.unsubscribe()
-        }
-        this.getPublishedData()
-    });
-      // this.router.navigate([this.entity, 'documents'])
-      // this.documentTypes = res;
-    }, (err) => {
-      this.router.navigate([this.entity, 'documents'])
+        this.mySubscription = interval(5000).subscribe((x) => {
+          console.log('xxx', x);
+          if (x == 12) {
+            this.router.navigate([this.entity, 'documents']);
+            this.toastMsg.error(
+              'error',
+              'TAKING_MORE_TIME_SOMETHING_WENT_WRONG'
+            );
+            this.mySubscription.unsubscribe();
+          }
+          this.getPublishedData();
+        });
+        // this.router.navigate([this.entity, 'documents'])
+        // this.documentTypes = res;
+      },
+      (err) => {
+        this.router.navigate([this.entity, 'documents']);
 
-      // this.toastMsg.error('error', err.error.params.errmsg)
-      console.log('error', err)
-    });
+        // this.toastMsg.error('error', err.error.params.errmsg)
+        console.log('error', err);
+      }
+    );
   }
 
-  getPublishedData(){
+  getPublishedData() {
     this.generalService.getData(this.entity).subscribe((res) => {
-      console.log('res', res)
-      var document = res[0][this.policyName].filter(doc => {
-        return doc.osid === this.attestationOSID
-      })
-      console.log("document", document);
-      if(document[0]._osState == "PUBLISHED"){
-        this.mySubscription.unsubscribe()
-        this.router.navigate([this.entity, 'documents'])
+      console.log('res', res);
+      var document = res[0][this.policyName].filter((doc) => {
+        return doc.osid === this.attestationOSID;
+      });
+      console.log('document', document);
+      if (document[0]._osState == 'PUBLISHED') {
+        this.mySubscription.unsubscribe();
+        this.router.navigate([this.entity, 'documents']);
       }
     });
   }
@@ -365,5 +399,4 @@ export class AddDocumentComponent implements OnInit {
     this.verify = false;
     // this.router.navigate(['documents'])
   }
-
 }
